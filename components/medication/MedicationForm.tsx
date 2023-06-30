5efce5042d647214fe78b2888e22117bc5e058f1
Alvin Quach <alvinwquach@gmail.com>
2023-06-29 19:03:51 -0700

    Create dynamic form

--- components/medication/MedicationForm.tsx
+++ components/medication/MedicationForm.tsx
@@ -1,17 +1,19 @@
 import { useEffect, useState } from "react";
-
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
-interface Medication {
-  id: number;
-  medicationId: number;
-  name: string;
+export type Medication = {
   action: string;
+  id: number;
+  medicationId: number | "";
+  name: string;
+  dose?: number;
+  doseUnit?: string;
+  logTime?: string;
   date: Date;
   time: string;
-}
+};
 
 const schema = yup.object().shape({
   medicationId: yup.string().required(),
@@ -20,16 +22,23 @@ const schema = yup.object().shape({
 });
 
 interface MedicationFormProps {
-  onSubmit: (data: Medication, mode: "log" | "edit") => void;
+  onSubmit: (data: Medication) => void;
   defaultValues?: Medication;
-  mode: "log" | "edit";
 }
 
-export const MedicationForm = ({
-  onSubmit,
-  defaultValues,
-  mode,
-}: MedicationFormProps) => {
+export const MedicationForm = ({ onSubmit, defaultValues }: MedicationFormProps) => {
+  const [availableMedication, setAvailableMedication] = useState<Medication[]>(
+    []
+  );
+
+  const [showAddNewMedicine, setShowAddNewMedicine] = useState(false);
+
+  const [isNewMedication, setIsNewMedication] = useState(false);
+
+  const toggleAddNewMedicine = () => {
+    setShowAddNewMedicine(!showAddNewMedicine);
+  };
+
   const {
     register,
     handleSubmit,
@@ -39,10 +48,6 @@ export const MedicationForm = ({
     resolver: yupResolver(schema),
     defaultValues,
   });
-
-  const [availableMedication, setAvailableMedication] = useState<Medication[]>(
-    []
-  );
 
   const getAvailableMedication = async () => {
     const response = await fetch("/api/medication", {
@@ -64,7 +69,7 @@ export const MedicationForm = ({
 
   const handleFormSubmit = async (data: Medication) => {
     await saveMedicationToDatabase({ ...data, action: "ADD_MEDICATION_LOG" });
-    onSubmit(data, mode);
+    onSubmit(data);
   };
 
   useEffect(() => {
@@ -72,48 +77,136 @@ export const MedicationForm = ({
     reset(defaultValues);
   }, [reset, defaultValues]);
 
+  const handleMedicationChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    if (value === "add_new") {
+      toggleAddNewMedicine();
+      setIsNewMedication(true);
+    } else {
+      const selectedMedication = availableMedication.find(
+        (medication) => medication.id === parseInt(value)
+      );
+      reset({ ...defaultValues, medicationId: selectedMedication?.id });
+      setIsNewMedication(false);
+    }
+  };
   return (
     <form onSubmit={handleSubmit(handleFormSubmit)}>
-      <label className="text-base font-medium text-gray-900">
-        Medication:
-        <select
-          {...register("medicationId")}
-          className="ml-2 text-base font-normal text-gray-900"
-        >
-          {availableMedication.map((medication) => (
-            <option key={medication.id} value={medication.id}>
-              {medication.name}
-            </option>
-          ))}
-        </select>
-        {errors.name && <span className="block">This field is required</span>}
-      </label>
-      <br />
-      <label className="text-base font-medium text-gray-900">
-        Date Taken:
-        <input
-          type="date"
-          className="ml-2 text-base font-normal text-gray-900"
-          {...register("date")}
-        />
-        {errors.date && <span className="block">This field is required</span>}
-      </label>
-      <br />
-      <label className="text-base font-medium text-gray-900">
-        Time Taken:
-        <input
-          type="time"
-          className="ml-2 text-base font-normal text-gray-900"
-          {...register("time")}
-        />
-        {errors.time && <span className="block">This field is required</span>}
-      </label>
-      <br />
+      {showAddNewMedicine ? (
+        <>
+          <label className="text-base font-medium text-gray-900">
+            Medication Name:
+            <input
+              type="text"
+              className="ml-2 text-base font-normal text-gray-900"
+              {...register("name", { required: true })}
+            />
+            {errors.name && (
+              <span className="block">This field is required</span>
+            )}
+          </label>
+          <br />
+          <label className="text-base font-medium text-gray-900">
+            Dose:
+            <input
+              type="number"
+              className="ml-2 text-base font-normal text-gray-900"
+              {...register("dose", { required: true })}
+            />
+            {errors.dose && (
+              <span className="block">This field is required</span>
+            )}
+          </label>
+          <br />
+          <label className="text-base font-medium text-gray-900">
+            Dose Unit:
+            <input
+              type="text"
+              className="ml-2 text-base font-normal text-gray-900"
+              {...register("doseUnit", { required: true })}
+            />
+            {errors.doseUnit && (
+              <span className="block">This field is required</span>
+            )}
+          </label>
+          <br />
+          <label className="text-base font-medium text-gray-900">
+            Log Time:
+            <input
+              type="time"
+              className="ml-2 text-base font-normal text-gray-900"
+              {...register("logTime", { required: true })}
+            />
+            {errors.logTime && (
+              <span className="block">This field is required</span>
+            )}
+          </label>
+          <br />
+          <button
+            type="button"
+            className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 mt-4 rounded"
+            onClick={toggleAddNewMedicine}
+          >
+            Cancel
+          </button>
+        </>
+      ) : (
+        <>
+          <label className="text-base font-medium text-gray-900">
+            Medication:
+            <select
+              {...register("medicationId")}
+              className="ml-2 text-base font-normal text-gray-900"
+              onChange={handleMedicationChange}
+            >
+              <option disabled>Find or add medicine</option>
+              <option value="add_new">+ Add new medicine</option>
+              {availableMedication.map((medication) => (
+                <option
+                  key={medication.id}
+                  value={medication.id}
+                  selected={defaultValues?.medicationId === medication.id}
+                >
+                  {medication.name}
+                </option>
+              ))}
+            </select>
+            {errors.medicationId && (
+              <span className="block">This field is required</span>
+            )}
+          </label>
+          <br />
+          <label className="text-base font-medium text-gray-900">
+            Date Taken:
+            <input
+              type="date"
+              className="ml-2 text-base font-normal text-gray-900"
+              {...register("date", { required: true })}
+            />
+            {errors.date && (
+              <span className="block">This field is required</span>
+            )}
+          </label>
+          <br />
+          <label className="text-base font-medium text-gray-900">
+            Time Taken:
+            <input
+              type="time"
+              className="ml-2 text-base font-normal text-gray-900"
+              {...register("time", { required: true })}
+            />
+            {errors.time && (
+              <span className="block">This field is required</span>
+            )}
+          </label>
+          <br />
+        </>
+      )}
       <button
         type="submit"
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 mt-4 rounded"
       >
-        {mode === "log" ? <span>Log Medication </span> : <span>Update</span>}
+        {isNewMedication ? "Save" : "Log"}
       </button>
     </form>
   );
