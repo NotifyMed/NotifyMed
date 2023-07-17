d7e17f28ada8ffc90d8b682f203d9c06c1b789cb
Alvin Quach <alvinwquach@gmail.com>
2023-07-16 22:31:46 -0700

    Create footer

---
+++ components/global/Footer.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-teal-900 bottom-0 p-10">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+        <div className="flex-none lg:flex">
+          <div className="flex flex-col flex-1 pb-4">
+            <div className="pb-3 text-lg tracking-tight font-title text-white">
+              Notify Med
+            </div>
+            <div className="pb-3 text-lg tracking-tight text-white">
+              Your Trusted Medication Companion
+            </div>
+            <div className="pb-3 text-lg tracking-tight text-white">
+              Since 2023
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
