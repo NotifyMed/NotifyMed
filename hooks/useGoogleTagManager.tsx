88cb54f364e2743ae8a53732d96d1b8c7a5fe27f
Alvin Quach <alvinquach@Alvins-MacBook-Air-2.local>
2023-07-26 14:25:25 -0700

    Add Google Tag Manager

---
+++ hooks/useGoogleTagManager.tsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtm from "lib/gtm";
+
+const useGoogleTagManager = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    if (process.env.NODE_ENV !== "production") return;
+
+    const handleRouteChange = (url: string) => {
+      gtm.pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+};
+
+export default useGoogleTagManager;
