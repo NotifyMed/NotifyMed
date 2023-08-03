a7e073358fc090ebd4a05ac98a0a826cc463f88e
Alvin Quach <alvinquach@Alvins-MacBook-Air-2.local>
2023-08-03 15:11:15 -0700

    Add Next SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,12 @@
 import "@/styles/globals.css";
+import { Inter } from "next/font/google";
 import type { AppProps } from "next/app";
+import { useEffect } from "react";
 import { SessionProvider } from "next-auth/react";
 import Layout from "@/components/layout/Layout";
-import { Inter } from "next/font/google";
-import useGoogleTagManager from "../hooks/useGoogleTagManager";
 import Script from "next/script";
-import { useEffect } from "react";
+import { DefaultSeo } from "next-seo";
+import useGoogleTagManager from "../hooks/useGoogleTagManager";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,41 @@ export default function App({
 
   return (
     <SessionProvider session={session} refetchInterval={5 * 60}>
+      <DefaultSeo
+        title="Notify Med - Medication Tracker and Reminder"
+        description="Never forget to take your medication again!"
+        canonical="https://notifymed.com/"
+        openGraph={{
+          url: "https://notifymed.com/",
+          title: "Notify Med - Medication Tracker and Reminder",
+          description: "Never miss a medication again!",
+          site_name: "Notify Med",
+          type: "website",
+          locale: "en_US",
+        }}
+        additionalMetaTags={[
+          {
+            name: "keywords",
+            content: "medication, reminder, tracker,",
+          },
+          {
+            name: "author",
+            content: "Notify Med",
+          },
+          {
+            property: "og:type",
+            content: "website",
+          },
+          {
+            property: "og:locale",
+            content: "en_US",
+          },
+          {
+            property: "og:site_name",
+            content: "Notify Med",
+          },
+        ]}
+      />
       {process.env.NODE_ENV === "production" && (
         <Script
           src={`https://www.googletagmanager.com/gtm.js?id=${process.env.NEXT_PUBLIC_GOOGLE_TAG_MANAGER_ID}`}
