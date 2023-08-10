9f4a467e48b77ef03e393dc89f53bed687d57e56
Alvin Quach <alvinquach@Alvins-MacBook-Air-2.local>
2023-08-10 14:58:43 -0700

    Fix user api

--- pages/api/user.ts
+++ pages/api/user.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
-import UserController from "@/src/Controllers/UserController";
+import knex from "@/src/knex/knex";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   switch (req.method) {
@@ -22,43 +22,60 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
 }
 
 async function addUser(req: NextApiRequest, res: NextApiResponse) {
-  const data = {
-    ...req.body.user,
-  };
-
-  const newUser = await UserController.AddUser(data);
-  if (newUser != null) return res.status(200).json(newUser);
-  return res.status(400).json({ error: "Error adding user" });
+  try {
+    let knexResponse = await knex("users")
+      .insert({
+        email: req.body.email,
+        password: req.body.password,
+        phone: req.body.phone,
+      })
+      .returning("*");
+    return res.status(200).json(knexResponse);
+  } catch (e) {
+    return res.status(400).json({ error: e });
+  }
 }
 
 async function getUser(req: NextApiRequest, res: NextApiResponse) {
-  const data = {
-    ...req.body.user,
-  };
-
-  const user = await UserController.GetUser(data);
-  if (user != null) return res.status(200).json(user);
-  return res.status(400).json({ error: "Error retrieving user" });
+  try {
+    let email = req.query.email;
+    let knexResponse = await knex("users")
+      .modify((qb) => {
+        email && qb.where({ email });
+        qb.where({ isDeleted: false });
+      })
+      .select("id", "email", "phone")
+      .first();
+    return res.status(200).json(knexResponse);
+  } catch (e) {
+    return res.status(400).json({ error: e });
+  }
 }
 
 async function updateUser(req: NextApiRequest, res: NextApiResponse) {
-  const data = {
-    ...req.body.user,
-  };
+  console.log(req.body.phone);
+  try {
+    await knex("users")
+      .where({ email: req.body.email })
+      .update({ phone: req.body.phone })
+      .returning("*");
+    return res
 
-  const updatedUser = await UserController.UpdateUser(data);
-
-  if (updatedUser != null) return res.status(200).json(updatedUser);
-  return res.status(400).json({ error: "Error updating user" });
+      .status(200)
+      .json({ message: "Phone number updated successfully" });
+  } catch (e) {
+    return res.status(400).json({ error: e });
+  }
 }
 
 async function deleteUser(req: NextApiRequest, res: NextApiResponse) {
-  const data = {
-    ...req.body.user,
-  };
-
-  const deletedUser = await UserController.DeleteUser(data);
-
-  if (deletedUser != null) return res.status(200).json(deletedUser);
-  return res.status(400).json({ error: "Error deleting user" });
+  try {
+    let knexResponse = await knex("users")
+      .where({ email: req.query.email })
+      .update({ isDeleted: true })
+      .returning("*");
+    return res.status(200).json(knexResponse);
+  } catch (e) {
+    return res.status(400).json({ error: e });
+  }
 }
