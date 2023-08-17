1bae1dca7a9d0d47fb2d6e0436116d804beb9ea8
Alvin Quach <alvinquach@Alvins-MacBook-Air-2.local>
2023-08-17 14:01:15 -0700

    Add path to middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,9 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/pages/api/user", "/pages/api/medication"],
+  matcher: [
+    "/pages/api/user",
+    "/pages/api/medication",
+    "/pages/api/auth/[...nextauth]",
+  ],
 };
