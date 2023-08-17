c92856a2bcf4f624b0906082a8d037b065870c6c
Alvin Quach <alvinquach@Alvins-MacBook-Air-2.local>
2023-08-17 11:47:27 -0700

    Update middleware

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,5 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-  matcher: ["/pages/api/:path*", "/pages/account", "/pages/schedule"],
+  matcher: ["/pages/api/:path*"],
 };
