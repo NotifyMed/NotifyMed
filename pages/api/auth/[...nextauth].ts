d0580105ad5119d47c0a9fb48591320a4e3d039e
Alvin Quach <alvinquach@Alvins-Air-2.askey.com>
2023-08-19 16:56:45 -0700

    Add max age to session

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -12,6 +12,7 @@ export const authOptions: NextAuthOptions = {
   secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: "jwt",
+    maxAge: 60 * 60 * 24,
   },
   // secret: process.env.JWT_SECRET,
   callbacks: {
