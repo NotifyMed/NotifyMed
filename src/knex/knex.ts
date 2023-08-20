470a7abbfeea3fd36ea18b0f798f6bcfc412755a
Alvin Quach <alvinquach@Alvins-MacBook-Air-2.local>
2023-08-19 21:32:39 -0700

    Fix config

--- src/knex/knex.ts
+++ src/knex/knex.ts
@@ -1,3 +1,5 @@
+const { parse } = require("pg-connection-string"); // Import the pg-connection-string package
+
 import Knex from "knex";
 import knexConfig from "../../knexfile";
 export default Knex(knexConfig.development);
