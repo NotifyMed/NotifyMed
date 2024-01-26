4b9ecaa464f2c4d0063284421418d3e555f515bc
Alvin Quach <alvinwquach@gmail.com>
2024-01-26 00:16:41 -0800

    Update config

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,8 @@ module.exports = {
   development: {
     client: "postgresql",
     connection: {
+      host: "localhost",
+      port: 5432,
       database: process.env.DB_DATABASE,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
