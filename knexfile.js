fdfe50ed738b23ebf5e50ce64693a805c48b921e
Alvin Quach <alvinwquach@gmail.com>
2024-01-25 23:59:02 -0800

    Fix config

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,11 @@ module.exports = {
   development: {
     client: "pg",
     connection: {
+      host: "137.184.122.12",
+      port: 5432,
       database: "notifymed",
       user: "postgres",
       password: "postgres",
-      port: 5432,
     },
     pool: {
       min: 2,
@@ -17,5 +18,6 @@ module.exports = {
       directory: "./src/knex/migrations",
       tableName: "knex_migrations",
     },
+    ssl: config["DB_SSL"] ? { rejectUnauthorized: false } : false,
   },
 };
