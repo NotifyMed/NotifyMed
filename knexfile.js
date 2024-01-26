fd8b0f4115aee4672bcc6cc5e9e1390d3af02bcc
Alvin Quach <alvinwquach@gmail.com>
2024-01-26 00:23:30 -0800

    Update config

--- knexfile.js
+++ knexfile.js
@@ -2,21 +2,22 @@ require("dotenv").config();
 
 module.exports = {
   development: {
-    client: "postgresql",
+    client: "pg",
     connection: {
-      host: "137.184.122.12",
-      port: 5432,
       database: process.env.DB_DATABASE,
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
     },
     pool: {
-      min: 2,
-      max: 10,
+      min: 0,
+      max: 95,
     },
     migrations: {
       directory: "./src/knex/migrations",
       tableName: "knex_migrations",
+    },
+    seeds: {
+      directory: "./src/knex/seeds",
     },
   },
 };
